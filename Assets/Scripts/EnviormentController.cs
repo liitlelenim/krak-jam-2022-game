2d2fd34cc5371f77a102b8284cbdb27d20a5cf05
GracjanMWitos <gracjan-witos@wp.pl>
2022-01-28 00:46:13 +0100

    Hacking mode nearly finished

--- Assets/Scripts/EnviormentController.cs
+++ Assets/Scripts/EnviormentController.cs
@@ -1,17 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnviormentController : MonoBehaviour
 {
     #region Assignment
-    Transform bluePlatform;
     EnviormentController secondBit;
     EnviormentController thirdBit;
+    EnviormentController fourthBit;
     HackingMode hackingMode;
     void Awake()
     {
-        bluePlatform = GameObject.Find("BluePlatform").transform;
         hackingMode = GameObject.FindGameObjectWithTag("Player").GetComponent<HackingMode>();
     }
     #endregion Assignment
@@ -81,28 +78,74 @@ void EnviormentControl()
         #region Platform
         if (platformsRotation)
         {
+            float speed = 5f;
             secondBit = GameObject.Find("BlueRotationSecond").GetComponent<EnviormentController>();
+            Transform bluePlatform = GameObject.Find("BluePlatform").transform;
+
             if (bitEquelsOne && secondBit.bitEquelsOne)
             {
-                bluePlatform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, 0f, 180f), 5f);
+                bluePlatform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, 0f, 180f), speed);
             }
             else if (bitEquelsOne && !secondBit.bitEquelsOne)
             {
-                bluePlatform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, 0f, -90f), 5f);
+                bluePlatform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, 0f, -90f), speed);
             }
             else if (!bitEquelsOne && secondBit.bitEquelsOne)
             {
-                bluePlatform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, 0f, 90f), 5f);
+                bluePlatform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, 0f, 90f), speed);
             }
             else
             {
-                bluePlatform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, 0f, 0f), 5f);
+                bluePlatform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, 0f, 0f), speed);
+            }
+
+            thirdBit = GameObject.Find("GreenRotationFirst").GetComponent<EnviormentController>();
+            fourthBit = GameObject.Find("GreenRotationSecond").GetComponent<EnviormentController>();
+            Transform greenPlatform = GameObject.Find("GreenPlatform").transform;
+            if (thirdBit.bitEquelsOne && fourthBit.bitEquelsOne)
+            {
+                greenPlatform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, 0f, 180f), speed);   
+            }
+            else if (thirdBit.bitEquelsOne && !fourthBit.bitEquelsOne)
+            {
+                greenPlatform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, 0f, -90f), speed);
+            }
+            else if (!thirdBit.bitEquelsOne && fourthBit.bitEquelsOne)
+            {
+                greenPlatform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, 0f, 90f), speed);
+            }
+            else
+            {
+                greenPlatform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, 0f, 0f), speed);
             }
         }
 
         if (platformsPosition)
         {
-            
+            GameObject[] yellowPos_1 = GameObject.FindGameObjectsWithTag("YellowPos1");
+            GameObject[] yellowPos_2 = GameObject.FindGameObjectsWithTag("YellowPos2");
+            GameObject[] yellowPlaftorm = GameObject.FindGameObjectsWithTag("YellowPlatform");
+
+            for (int i = 0; i < yellowPlaftorm.Length; i++) 
+            {
+                if (bitEquelsOne)
+                    yellowPlaftorm[i].transform.position = yellowPos_2[i].transform.position;
+                else
+                    yellowPlaftorm[i].transform.position = yellowPos_1[i].transform.position;
+            }
+
+            secondBit = GameObject.Find("BrownChangePosition").GetComponent<EnviormentController>();
+            GameObject[] brownPos_1 = GameObject.FindGameObjectsWithTag("BrownPos1");
+            GameObject[] brownPos_2 = GameObject.FindGameObjectsWithTag("BrownPos2");
+            GameObject[] brownPlaftorm = GameObject.FindGameObjectsWithTag("BrownPlatform");
+
+            for (int i = 0; i < yellowPlaftorm.Length; i++)
+            {
+                if (secondBit.bitEquelsOne)
+                    brownPlaftorm[i].transform.position = brownPos_2[i].transform.position;
+                else
+                    brownPlaftorm[i].transform.position = brownPos_1[i].transform.position;
+            }
         }
         #endregion Platform
         #region Gravity
