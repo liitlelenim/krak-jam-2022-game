6a8258e815e0da8d65696ededc50ab9519046970
liitlelenim <jakubblaszczyk20@gmail.com>
2022-01-28 22:12:42 +0100

    add music playing system

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] musicClips;
+    [SerializeField] private int levelToLoad;
+    private AudioSource _audioSource;
+    private int _currentMusic = -1;
+
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        DontDestroyOnLoad(this);
+        PlayNewSong();
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    private IEnumerator WaitUntilEndOfTheSong(int songLength)
+    {
+        yield return new WaitForSeconds(songLength);
+        PlayNewSong();
+    }
+
+    private void PlayNewSong()
+    {
+        _currentMusic++;
+        if (_currentMusic > musicClips.Length - 1)
+        {
+            _currentMusic = 0;
+        }
+
+        _audioSource.clip = musicClips[_currentMusic];
+        _audioSource.PlayDelayed(3f);
+        StartCoroutine(WaitUntilEndOfTheSong((int) _audioSource.clip.length));
+    }
+}
