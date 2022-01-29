af797217adfe22d5059cf6b4d253014a29cc650d
liitlelenim <jakubblaszczyk20@gmail.com>
2022-01-29 21:08:08 +0100

    add going to next level after getting cartridges

--- Assets/Scripts/CartridgeManager.cs
+++ Assets/Scripts/CartridgeManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CartridgeManager : MonoBehaviour
 {
@@ -54,6 +55,10 @@ public class CartridgeManager : MonoBehaviour
                 _soundsManager.PlaySound(3);
             }
 
+            if (OwnedCartridgesAmount >= CartdrigeToGetInLevel)
+            {
+                LevelManager.LoadNextLevel();
+            }
             Destroy(other.gameObject);
         }
     }
