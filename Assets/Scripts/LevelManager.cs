a0c4afa2dd1cab3fae74de05e5c15de90571635a
liitlelenim <jakubblaszczyk20@gmail.com>
2022-01-29 16:01:21 +0100

    add level manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class LevelManager 
+{
+   public static void RestartLevel()
+   {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+   }
+
+   public static void LoadNextLevel()
+   {
+      SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+   }
+
+   public static void LoadLevel(int buildIndex)
+   {
+      SceneManager.LoadScene(buildIndex);
+   }
+   public static void LoadMenu()
+   {
+      throw new NotImplementedException("Implement when menu is added");
+   }  
+}
