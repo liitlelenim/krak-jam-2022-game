a8ea9ea79f3cc61d64006c32a6c05381ea54d830
liitlelenim <jakubblaszczyk20@gmail.com>
2022-01-29 11:45:08 +0100

    improve death mechanics and running particles

--- Assets/Scripts/PlayerLifeController.cs
+++ Assets/Scripts/PlayerLifeController.cs
@@ -6,11 +6,43 @@ using UnityEngine.SceneManagement;
 
 public class PlayerLifeController : MonoBehaviour
 {
-    public void Death()
+    [SerializeField] private Sprite deathSprite;
+    private PlayerMovement _playerMovement;
+    private HackingMode _hackingMode;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+    private BoxCollider2D _boxCollider;
+    private void Awake()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        _playerMovement = GetComponent<PlayerMovement>();
+        _hackingMode = GetComponent<HackingMode>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
+        _boxCollider = GetComponent<BoxCollider2D>();
     }
 
+    private void Update()
+    {
+
+    }
+
+    public void Death()
+    {
+        _playerMovement.enabled = false;
+        _hackingMode.enabled = false;
+        _animator.enabled = false;
+        _spriteRenderer.sprite = deathSprite;
+        _boxCollider.size = new Vector2(1.75f, 0.85f);
+        _boxCollider.offset = new Vector2(0.007746935f, -0.012362f);
+        StartCoroutine(RestartSceneWithDelay());
+    }
+
+    private IEnumerator RestartSceneWithDelay()
+    {
+        yield return new WaitForSeconds(5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.GetComponent<PlayerKiller>() != null)
