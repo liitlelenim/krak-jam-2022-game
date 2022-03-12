34ef0000632eb6d97b83eabd3657e2d274ea1511
liitlelenim <jakubblaszczyk20@gmail.com>
2022-03-12 17:40:16 +0100

    add jump feature to player default movement mode

--- Assets/Code/Player/Movement/PlayerDefaultMovementMode.cs
+++ Assets/Code/Player/Movement/PlayerDefaultMovementMode.cs
@@ -5,42 +5,54 @@ namespace Player.Movement
 {
     public class PlayerDefaultMovementMode : PlayerMovementMode
     {
-        [Header("Horizontal movement settings")] 
-        [SerializeField] private float speed = 5f;
+        [Header("Horizontal movement settings")] [SerializeField]
+        private float speed = 5f;
 
         [SerializeField] private AnimationCurve horizontalMovementCurve;
-        
+
         private int _horizontalMovementAxis = 0;
         private float _horizontalMovementTimer = 0.0f;
-        private PlayerControls _playerControls;
-        private Rigidbody2D _rigidbody2D;
+        [SerializeField] float accelerationLength = 1f;
 
-        [Header("Vertical movement settings")] 
-        [SerializeField] private float jumpForce;
+        [Header("Vertical movement settings")] [SerializeField]
+        private float jumpForce;
+
         [SerializeField] private float jumpCooldown;
         [SerializeField] private float maxJumpLength;
         [SerializeField] private float minJumpLength;
-        [SerializeField] private AnimationCurve jumpForceCurve;
+        [Range(0, 1)] [SerializeField] private float jumpCutOffFactor;
+        private float appliedCutOffFactor = 1;
+        private float _timeSinceBeingGrounded = 0;
         private float _timeSinceLastJump = 0;
         private float _currentJumpLength = 0;
         private bool _isJumping = false;
         private bool _shouldCutOffJump = false;
-        [Header("Gravity settings")]
-        [SerializeField] private float maxGravityMultiplier = 1.25f;
-        [SerializeField] private float initialAfterJumpGravity = -4f;
-        [SerializeField] private float gravityGrowthSpeed = 9f;
-        private float _currentGravity = 0;
+        private bool _alreadyCutOffJump = false;
+
+        [Header("Player comfort settings")] [SerializeField]
+        private float coyoteTimeDuration;
+
+        [SerializeField] private float jumpBufferingDuration;
+        private float _timeSinceLastJumpButtonPress;
+
+        private PlayerControls _playerControls;
+        private Rigidbody2D _rigidbody2D;
+        private GroundChecker[] _groundCheckers;
+
         protected override void Awake()
         {
             _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
             _playerControls = new PlayerControls();
+            _groundCheckers = GetComponentsInChildren<GroundChecker>();
+
             _playerControls.Movement.Horizontal.performed += ctx =>
             {
                 ChangeMovementAxis((int) ctx.ReadValue<float>());
             };
             _playerControls.Movement.Horizontal.canceled += _ => { ChangeMovementAxis(0); };
 
-            _playerControls.Movement.Jump.performed += _ => PerformJump();
+            _playerControls.Movement.Jump.performed += _ => _timeSinceLastJumpButtonPress = 0;
+
             _playerControls.Movement.Jump.canceled += _ => _shouldCutOffJump = true;
         }
 
@@ -48,66 +60,83 @@ namespace Player.Movement
         {
             _horizontalMovementTimer += Time.deltaTime;
             _timeSinceLastJump += Time.deltaTime;
+            _timeSinceBeingGrounded += Time.deltaTime;
+            _timeSinceLastJumpButtonPress += Time.deltaTime;
             if (_isJumping)
             {
                 _currentJumpLength += Time.deltaTime;
             }
-            else
+
+
+            foreach (GroundChecker checker in _groundCheckers)
             {
-                _currentGravity -= Time.deltaTime * gravityGrowthSpeed;
+                if (checker.IsGrounded)
+                {
+                    _timeSinceBeingGrounded = 0f;
+                    break;
+                }
             }
 
-            if (_shouldCutOffJump && _currentJumpLength > minJumpLength)
+            if (_timeSinceLastJumpButtonPress < jumpBufferingDuration)
             {
-                EndJump();
+                PerformJump();
+            }
+
+            if (_shouldCutOffJump && _currentJumpLength > minJumpLength && !_alreadyCutOffJump)
+            {
+                if (Math.Sign(_rigidbody2D.velocity.y) == Math.Sign(transform.up.y))
+                {
+                    _alreadyCutOffJump = true;
+                    _rigidbody2D.velocity =
+                        new Vector2(_rigidbody2D.velocity.x, _rigidbody2D.velocity.y * jumpCutOffFactor);
+                }
             }
         }
 
         private void FixedUpdate()
         {
-            _currentGravity = Mathf.Clamp(_currentGravity, Physics2D.gravity.y * maxGravityMultiplier,0 );
             if (_currentJumpLength > maxJumpLength)
             {
                 EndJump();
             }
-            float verticalAxis = _isJumping ? CalculateCurrentJumpVelocity()  : _currentGravity;
-            
-            _rigidbody2D.velocity
-                = Vector2.right * (speed * _horizontalMovementAxis *
-                                   horizontalMovementCurve.Evaluate(_horizontalMovementTimer))
-                + Vector2.up * verticalAxis;
+
+            _rigidbody2D.velocity =
+                new Vector2(_horizontalMovementAxis * speed *
+                            horizontalMovementCurve.Evaluate(_horizontalMovementTimer / accelerationLength),
+                    _rigidbody2D.velocity.y);
         }
+
         private void ChangeMovementAxis(int axis)
         {
             _horizontalMovementAxis = axis;
             _horizontalMovementTimer = 0;
         }
-        
+
         private void PerformJump()
         {
             if (CanJump())
             {
-                _shouldCutOffJump = false;
+                
                 _timeSinceLastJump = 0;
                 _isJumping = true;
+                _alreadyCutOffJump = false;
+                _shouldCutOffJump = false;
+                _rigidbody2D.AddForce(transform.up*jumpForce);
             }
         }
 
         private void EndJump()
         {
-            _currentGravity = -4;
-            _currentJumpLength = 0;
-            _isJumping = false;
-            _shouldCutOffJump = false;
+            if (_isJumping)
+            {
+                _isJumping = false;
+                _currentJumpLength = 0;
+            }
         }
 
         private bool CanJump()
         {
-            return _timeSinceLastJump > jumpCooldown  && !_isJumping;
-        }
-        private float CalculateCurrentJumpVelocity()
-        {
-            return jumpForceCurve.Evaluate(_currentJumpLength/maxJumpLength)*jumpForce;
+            return _timeSinceLastJump > jumpCooldown && _timeSinceBeingGrounded < coyoteTimeDuration && !_isJumping;
         }
         
         private void OnEnable()
