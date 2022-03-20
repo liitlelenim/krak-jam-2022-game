2c9e8e34f8c08bf5d7e6ecf201b2f15a65b8991a
liitlelenim <jakubblaszczyk20@gmail.com>
2022-03-20 15:15:08 +0100

    add inertia system and clamping falling speed

--- Assets/Code/Player/Movement/PlayerDefaultMovementMode.cs
+++ Assets/Code/Player/Movement/PlayerDefaultMovementMode.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Player.Movement
@@ -16,6 +17,7 @@ namespace Player.Movement
         private float _horizontalMovementTimer = 0.0f;
         [SerializeField] float accelerationLength = 1f;
         [SerializeField] float decelerationLength = 1f;
+        private int _decelerationAxis = 0;
         private bool _shouldDecelerate;
         private float _decelerateTimer;
 
@@ -26,12 +28,15 @@ namespace Player.Movement
         [SerializeField] private float maxJumpLength;
         [SerializeField] private float minJumpLength;
         [Range(0, 1)] [SerializeField] private float jumpCutOffFactor;
+        [SerializeField] private float maxFallingSpeed = 11;
+        [SerializeField] private float midAirDirectionChangeSpeed = 15f;
         private float _timeSinceBeingGrounded = 0;
         private float _timeSinceLastJump = 0;
         private float _currentJumpLength = 0;
         private bool _isJumping = false;
         private bool _shouldCutOffJump = false;
         private bool _alreadyCutOffJump = false;
+        private float _midAirMomentum = 0f;
 
         [Header("Player comfort settings")] [SerializeField]
         private float coyoteTimeDuration;
@@ -77,14 +82,20 @@ namespace Player.Movement
                 _currentJumpLength += Time.deltaTime;
             }
 
-
+            bool isGroundedDuringCurrentFrame = false;
             foreach (GroundChecker checker in _groundCheckers)
             {
                 if (checker.IsGrounded)
                 {
-                    _timeSinceBeingGrounded = 0f;
+                    isGroundedDuringCurrentFrame = true;
+                    OnBeingGrounded();
                     break;
                 }
+            }
+
+            if (_timeSinceBeingGrounded == 0f && isGroundedDuringCurrentFrame == false)
+            {
+                OnBeingUngrounded();
             }
 
             if (_timeSinceLastJumpButtonPress < jumpBufferingDuration)
@@ -100,6 +111,19 @@ namespace Player.Movement
                     _rigidbody2D.velocity =
                         new Vector2(_rigidbody2D.velocity.x, _rigidbody2D.velocity.y * jumpCutOffFactor);
                 }
+            }
+        }
+
+        private void OnBeingGrounded()
+        {
+            _timeSinceBeingGrounded = 0f;
+        }
+
+        private void OnBeingUngrounded()
+        {
+            if (!_isJumping)
+            {
+                _midAirMomentum = _horizontalMovementAxis;
             }
         }
 
@@ -120,9 +144,49 @@ namespace Player.Movement
                 curveFactor = accelerationCurve.Evaluate(_horizontalMovementTimer / accelerationLength);
             }
 
+            float verticalVelocity = _rigidbody2D.velocity.y;
+            if (verticalVelocity * Math.Sign(-transform.up.y) > maxFallingSpeed)
+            {
+                verticalVelocity = maxFallingSpeed * Math.Sign(-transform.up.y);
+            }
+
+            float horizontalVelocity = 0;
+            if (_timeSinceBeingGrounded == 0)
+            {
+                if (_shouldDecelerate)
+                {
+                    horizontalVelocity = _decelerationAxis * speed * curveFactor;
+                }
+                else
+                {
+                    horizontalVelocity = _horizontalMovementAxis * speed * curveFactor;
+                }
+            }
+            else
+            {
+                if (_horizontalMovementAxis != 0)
+                {
+                    _midAirMomentum += Time.deltaTime * _horizontalMovementAxis * midAirDirectionChangeSpeed;
+                }
+                else
+                {
+                    if (Mathf.Abs(_midAirMomentum) <= 0.25f)
+                    {
+                        _midAirMomentum = 0;
+                    }
+                    else
+                    {
+                        _midAirMomentum -= Time.deltaTime * midAirDirectionChangeSpeed * Mathf.Sign(_midAirMomentum);
+                    }
+                }
+
+                _midAirMomentum = Mathf.Clamp(_midAirMomentum, -1f, 1f);
+
+                horizontalVelocity = speed * _midAirMomentum;
+            }
+
             _rigidbody2D.velocity =
-                new Vector2(_horizontalMovementAxis * speed * curveFactor,
-                    _rigidbody2D.velocity.y);
+                new Vector2(horizontalVelocity, verticalVelocity);
         }
 
         private void ChangeMovementAxis(int axis)
@@ -136,6 +200,8 @@ namespace Player.Movement
             }
             else
             {
+                _decelerationAxis = _horizontalMovementAxis;
+                _horizontalMovementAxis = 0;
                 _shouldDecelerate = true;
             }
         }
@@ -149,8 +215,10 @@ namespace Player.Movement
                 _alreadyCutOffJump = false;
                 _shouldCutOffJump = false;
                 _rigidbody2D.AddForce(transform.up * jumpForce);
+                _midAirMomentum = _horizontalMovementAxis;
             }
         }
+
 
         private void EndJump()
         {
